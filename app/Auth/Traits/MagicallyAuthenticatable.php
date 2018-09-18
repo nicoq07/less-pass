39fd8080b5c6b345ee834a9a5fedbd7a1ea1d6b6
nicoq07 <nicoq07@gmail.com>
2018-09-18 01:50:48 -0300

    Trait

---
+++ app/Auth/Traits/MagicallyAuthenticatable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Auth\Traits;
+
+use Mail;
+use App\Mail\MagicLoginRequested;
+use App\UserLoginToken;
+use App\Http\Controllers\Auth\MagicLoginController;
+
+
+trait MagicallyAuthenticatable
+{
+
+    public function storeToken()
+    {
+        $this->token()->delete();
+
+        $this->token()->create([
+            'token' => str_random(255)
+        ]);
+        return $this;
+    }
+
+    public function sendMagicLink(array $options)
+    {
+        Mail::to($this)->send(new MagicLoginRequested($this, $options));
+    }
+
+    public function token()
+    {
+        return $this->hasOne(UserLoginToken::class);
+    }
+
+
+}
+
