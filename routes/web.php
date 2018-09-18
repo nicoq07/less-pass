1d2fa37569f26fe9f656202847c8300d43582f8a
nicoq07 <nicoq07@gmail.com>
2018-09-18 01:49:20 -0300

    rutas

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,20 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/login/magic', 'Auth\MagicLoginController@show');
+
+Route::post('/login/magic', 'Auth\MagicLoginController@sendToken');
+
+
+Route::get('/login/magic/{token}', 'Auth\MagicLoginController@validateToken');
